0c123f07dccfb6037d7817045d58914507eaabaf
Turbovv <108888834+Turbovv@users.noreply.github.com>
2023-10-01 14:03:57 +0400

    add carousel/review_section

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,17 @@ import Header from './components/Header.vue'
 import About from './components/About.vue'
 import MenuItems from './components/MenuItems.vue'
 import IntroVideo from './components/IntroVideo.vue'
+import Review from './components/Review.vue'
+import Carousel from './components/Carousel.vue'
 
 export default {
     components: {
         Header,
         About,
         MenuItems,
-        IntroVideo
+        IntroVideo,
+        Review,
+        Carousel
     }
 }
 </script>
@@ -20,5 +24,18 @@ export default {
     <About />
     <MenuItems />
     <IntroVideo />
+    <Review />
+  <div class="carou">
+    <Carousel />
+  </div>
 </div>
 </template>
+
+<style scoped>
+.carou {
+    border:1px solid red;
+    justify-content: center;
+    max-width:80%;
+    margin: 0 auto;
+}
+</style>
---
+++ src/components/Review.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="food-menu">
+        <div class="food-menu-inner">
+            <p class="center food-menu-inner-text">Testimonials</p>
+            <h1 class="center food-menu-inner-text-h1">What they said</h1>
+            <div class="food-menu-center">
+                <div class="flex-wrap">
+                    <div class="single-food-item">
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </template>
+    
+
+    
+    <style scoped>
+    .food-menu-inner {
+        /* border: 1px solid red; */
+        margin: 0 auto;
+        position: relative;
+        top:200px;
+    }
+    
+    .food-menu-inner .food-menu-inner-text {
+        color: #d6ad86;
+        font-weight: 400;
+        font-family: "Montserrat", sans-serif;
+    }
+    
+    .food-menu-inner-text .food-menu-inner-text-h1 {
+        font-size: 40px;
+        color: #2c3033;
+        line-height: 1.25;
+        font-weight: 700;
+        position: relative;
+        z-index: 1;
+    }
+    
+    .food-menu-center {
+        display: grid;
+        justify-content: center;
+        position: relative;
+        margin-top:50px;
+    
+    }
+    .flex-wrap {
+        display:grid;
+        gap:100px;
+        grid-template-columns: auto auto;
+    }
+    .center {
+        justify-content: center;
+        display: grid;
+    }
+    
+    .single-food-item {
+        display: flex;
+        border: 1px solid #f3e6da;
+    }
+    body {
+        background-color: black;
+    }
+ 
+    </style>
+    
